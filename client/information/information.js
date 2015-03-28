433dbba6296d64f8e28f4b89c5c6edcce6d74690
Blake Nanakdewa <BlakeNanakdewa@14-109-237.guest.wireless.msu.edu>
2015-03-28 09:31:26 -0400

    fasfdsa

---
+++ client/information/information.js
@@ -0,0 +1,4 @@
+Template.information.helpers({
+
+
+});
---
+++ client/transportationModal/transportationModal.js
@@ -0,0 +1,15 @@
+Template.transportationModal.events({
+
+	'click .test': function(e){
+
+		var start = $('.start').val();
+		var end = $('.end').val();
+		console.log(start);
+		console.log(end);
+
+	
+
+	}
+
+
+});
