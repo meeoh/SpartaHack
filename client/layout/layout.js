6eda24a03140645981a33d8e9823974d9630d2f7
Blake Nanakdewa <BlakeNanakdewa@14-109-237.guest.wireless.msu.edu>
2015-03-28 14:12:49 -0400

    steps working

---
+++ client/layout/layout.js
@@ -0,0 +1,9 @@
+// Meteor.http.get("http://api.wolframalpha.com/v2/query?input=pi&appid=AW6G96-XA62Q386Y4", 
+// 	{"header": "Access-Control-Allow-Origin: *"}, function(error, result)
+// {
+// 	console.log(error);
+// })
+
+// // https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyCbtIooDUgve2uxChkXiKG6v1W3_fNv-Z4
+
+// // AW6G96-XA62Q386Y4
---
+++ server/server.js
@@ -0,0 +1,32 @@
+
+Meteor.methods({
+
+	what: function(){
+		//this.unblock();
+
+		var result = Meteor.http.call("GET","https://maps.googleapis.com/maps/api/directions/json?origin=14 osprey court ontario&destination=8 bird court cambridge ontario&key=AIzaSyCbtIooDUgve2uxChkXiKG6v1W3_fNv-Z4");
+
+		var distance = result.data.routes[0].legs[0].distance.text;
+		var time = result.data.routes[0].legs[0].duration.text;
+		var start = result.data.routes[0].legs[0].start_address;
+		var end = result.data.routes[0].legs[0].end_address;
+		// end_location
+
+		var i;
+		var length = result.data.routes[0].legs[0].steps.length;
+		for (i = 0; i < length; i = i + 1)
+		{
+			console.log(result.data.routes[0].legs[0].steps[i].html_instructions);
+		}
+
+		console.log(distance);
+		console.log(time);
+		console.log(start);
+		console.log(end);
+
+		// console.log(result);
+
+	}
+
+
+});
