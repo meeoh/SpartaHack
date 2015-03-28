2883e1336833e07afccadbee7a3b1acd72b3cc11
newgz <ian_nguyen95@hotmail.com>
2015-03-28 16:58:00 -0400

    Updated Html

--- client/information/information.js
+++ client/information/information.js
@@ -1,28 +1,11 @@
-Template.information.rendered = function(){
-
-	// Meteor.call('what', function(err, res){
-	// 	console.log(res);
-	// })
-
-	Meteor.call('what', function(err, data)
-	{
-		console.log(data);
-		Session.set("testing", data);
-
-		// Session.setPersistant("testing", data);
-	});
-
-};
-
 Template.information.helpers({
 
 	'starting': function() {
-		return Session.get("testing")[2];
+		return Session.get("start");
 	},
 	'ending': function() {
-		return Session.get("testing")[3];
+		return Session.get("end");
 	}
 
 
 });
-
